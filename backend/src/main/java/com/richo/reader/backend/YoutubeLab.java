3eddebd5f21eeaeb32bf0c11925cb7637de5621e
RichoDemus <richodemus@gmail.com>
2015-04-27 20:55:08 +0200

    Now able to get an entire playlist for a channel

--- backend/src/main/java/com/richo/reader/backend/YoutubeLab.java
+++ backend/src/main/java/com/richo/reader/backend/YoutubeLab.java
@@ -6,6 +6,7 @@ import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Channel;
 import com.google.api.services.youtube.model.ChannelListResponse;
 import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,13 +27,17 @@ public class YoutubeLab
 	public void getMyVideos() throws IOException
 	{
 		youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request -> {
-		}).setApplicationName("youtube-cmdline-search-sample").build();
+		}).setApplicationName("Richo-Reader").build();
 
 
 		final String apiKey = "AIzaSyChI7lMyLfc1ckOqcC-z2Oz-Lrq6d09x30";
 
 
-		final ChannelListResponse result = youtube.channels().list("contentDetails").setKey(apiKey).setForUsername("RichoDemus").execute();
+		final ChannelListResponse result = youtube.channels()
+				.list("contentDetails")
+				.setKey(apiKey)
+				.setForUsername("RichoDemus")
+				.execute();
 		final List<Channel> channels = result.getItems();
 		System.out.println(channels.size());
 		channels.forEach((chan) -> {
@@ -40,13 +45,28 @@ public class YoutubeLab
 			{
 				final String playListId = chan.getContentDetails().getRelatedPlaylists().getUploads();
 
-				youtube.channels().list("snippet").setKey(apiKey).setId(playListId).execute();
-				final List<PlaylistItem> snippets = youtube.playlistItems().list("snippet").setKey(apiKey).setPlaylistId(playListId).execute().getItems();
-				System.out.println(snippets.size());
-				snippets.forEach((item) ->
+				String nextPageToken = "";
+				do
 				{
-					System.out.println("Title: " + item.getSnippet().getTitle());
-				});
+					youtube.channels().list("snippet").setKey(apiKey).setId(playListId).execute();
+					final PlaylistItemListResponse playlistItemListResponse = youtube.playlistItems()
+							.list("snippet")
+							.setKey(apiKey)
+							.setPageToken(nextPageToken)
+							.setPlaylistId(playListId)
+							.setMaxResults(25L)
+							.execute();
+					nextPageToken = playlistItemListResponse.getNextPageToken();
+					final List<PlaylistItem> snippets = playlistItemListResponse.getItems();
+					System.out.println(snippets.size());
+					snippets.forEach((item) ->
+					{
+						System.out.println("Title: " + item.getSnippet().getTitle());
+						System.out.println("Upload date: " + item.getSnippet().getPublishedAt().toString());
+					});
+
+				} while (nextPageToken != null);
+
 
 
 			}
