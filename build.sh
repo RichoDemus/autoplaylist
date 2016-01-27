d3f3073a6c3ae46b3c5740c55f60e33874b3856f
RichoDemus <richodemus@gmail.com>
2016-01-27 18:49:05 +0100

    Made a simple build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#from http://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+JAR_FILE=${DIR}/web/target/web-1.0-SNAPSHOT.jar
+CONFIG_FILE=${DIR}/config.yaml
+CONTENT_DIR=${DIR}/docker/content/
+
+mvn clean install -T1C
+
+mkdir -p $CONTENT_DIR
+cp $JAR_FILE $CONTENT_DIR
+cp $CONFIG_FILE $CONTENT_DIR
+
+docker build -t rpi-reader ${DIR}/docker/
+
+rm $CONTENT_DIR/*
+rmdir $CONTENT_DIR
+echo "Done!"
+
