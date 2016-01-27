e6cc5ba84e6590e6762f61293689e2b572a20fcc
RichoDemus <richodemus@gmail.com>
2016-01-27 19:46:38 +0100

    Tokens now time out after 4 hours

--- web/src/main/java/com/richo/reader/web/dropwizard/GuiceModule.java
+++ web/src/main/java/com/richo/reader/web/dropwizard/GuiceModule.java
@@ -2,12 +2,14 @@ package com.richo.reader.web.dropwizard;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.name.Names;
 import com.richo.reader.backend.inject.BackendModule;
 import com.richo.reader.web.authentication.UserServiceBridge;
 import com.richodemus.dropwizard.jwt.UserService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.time.Duration;
 
 public class GuiceModule extends AbstractModule
 {
@@ -16,6 +18,7 @@ public class GuiceModule extends AbstractModule
 	{
 		install(new BackendModule());
 		bind(UserService.class).to(UserServiceBridge.class);
+		bind(Duration.class).annotatedWith(Names.named("tokenDuration")).toInstance(Duration.ofHours(4));
 	}
 
 	@Inject
