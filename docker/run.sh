fac7d91c51d1c3e5da4183db223ce09c8033fcfa
RichoDemus <richodemus@gmail.com>
2016-02-20 22:45:54 +0100

    Updated runscript witch changes made on the host

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --name reader -d --restart always -v /home/pi/applications/reader/data:/reader/data rpi-reader
+docker run --name reader -d --restart always --log-opt max-size=100m --log-opt max-file=1 -p 8081:8081 -p 5005:5005 -v /home/richo/applications/reader/data:/reader/data rpi-reader
 
