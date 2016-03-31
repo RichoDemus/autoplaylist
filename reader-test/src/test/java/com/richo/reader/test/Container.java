4350c158ad294661ea480e880f1be039ccd15878
RichoDemus <richodemus@gmail.com>
2016-03-31 23:12:44 +0200

    Created a container testing harness with a simple test

---
+++ reader-test/src/test/java/com/richo/reader/test/Container.java
@@ -0,0 +1,92 @@
+package com.richo.reader.test;
+
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+import com.spotify.docker.client.messages.HostConfig;
+import com.spotify.docker.client.messages.PortBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BooleanSupplier;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class Container implements AutoCloseable
+{
+	private final DefaultDockerClient docker;
+	private final String id;
+
+	public Container(String image, final Set<String> ports) throws Exception
+	{
+		if (!image.contains(":"))
+		{
+			image = image + ":latest";
+		}
+		docker = DefaultDockerClient.fromEnv().build();
+
+		final Map<String, List<PortBinding>> portBindings = new HashMap<>();
+		for (String port : ports)
+		{
+			List<PortBinding> hostPorts = new ArrayList<PortBinding>();
+			hostPorts.add(PortBinding.of("0.0.0.0", port));
+			portBindings.put(port, hostPorts);
+		}
+
+		final HostConfig hostConfig = HostConfig.builder().portBindings(portBindings).build();
+
+		// Create container with exposed ports
+		final ContainerConfig containerConfig = ContainerConfig.builder()
+				.hostConfig(hostConfig)
+				.image(image).exposedPorts(ports)
+				.build();
+
+		final ContainerCreation creation = docker.createContainer(containerConfig);
+		id = creation.id();
+
+		System.out.println("container id: " + id);
+
+		docker.startContainer(id);
+	}
+
+	public void awaitStartup(final BooleanSupplier supplier) throws Exception
+	{
+		testIfStartedUp(300, supplier);
+	}
+
+	private void testIfStartedUp(final int retriesLeft, final BooleanSupplier supplier) throws Exception
+	{
+		if (retriesLeft == 0)
+		{
+			fail("Container never started");
+		}
+		if (!isRunning(supplier))
+		{
+			Thread.sleep(10L);
+			testIfStartedUp(retriesLeft - 1, supplier);
+		}
+	}
+
+	private boolean isRunning(BooleanSupplier supplier)
+	{
+		try
+		{
+			return supplier.getAsBoolean();
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+
+	@Override
+	public void close() throws Exception
+	{
+		docker.stopContainer(id, 1);
+		docker.removeContainer(id);
+		docker.close();
+	}
+}
--- reader-test/src/test/java/com/richo/reader/test/StatusTestIT.java
+++ reader-test/src/test/java/com/richo/reader/test/StatusTestIT.java
@@ -1,17 +1,44 @@
 package com.richo.reader.test;
 
-import com.spotify.docker.client.DefaultDockerClient;
+import com.google.common.collect.Sets;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.HashSet;
+
+import static com.jayway.restassured.RestAssured.get;
 
 public class StatusTestIT
 {
-	@Test
-	public void failMeIT() throws Exception
-	{
-		DefaultDockerClient.fromEnv().build();
-		fail("Autofail");
+	private static final String PORT = "8080";
+	private static final String ADMIN_PORT = "8081";
+	private static final String PING = "http://localhost:" + ADMIN_PORT + "/ping";
+	private static final HashSet<String> PORTS = Sets.newHashSet(PORT, ADMIN_PORT);
 
+	private Container container;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		container = new Container("richodemus/reader", PORTS);
+		container.awaitStartup(this::testIfContainerStarted);
+	}
+
+	private boolean testIfContainerStarted()
+	{
+		return get(PING).then().extract().statusCode() == 200;
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		container.close();
+	}
+
+	@Test
+	public void shouldReturn200OK() throws Exception
+	{
+		get("http://localhost:8080/").then().assertThat().statusCode(200);
 	}
 }
