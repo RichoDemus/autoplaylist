229efcbdf1811e4bda31198b0055f9cfa6a5872f
RichoDemus <richodemus@gmail.com>
2016-03-31 21:57:54 +0200

    Can now build on both x64 and arm

--- build.sh
+++ build.sh
@@ -3,6 +3,20 @@
 #from http://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+CPU_ARCHITECTURE="$( uname -m )"
+
+case $CPU_ARCHITECTURE in
+x86_64)
+  echo "you have a 64bit processor"
+  ;;
+armv7l)
+  echo "you have an arm processor"
+  ;;
+*)
+  echo "Unsupported architecture: " $CPU_ARCHITECTURE
+  exit 1
+esac
+
 JAR_FILE=${DIR}/web/target/web-1.0-SNAPSHOT.jar
 CONFIG_FILE=${DIR}/config.yaml
 CONTENT_DIR=${DIR}/docker/content/
@@ -18,7 +32,15 @@ mkdir -p $CONTENT_DIR
 cp $JAR_FILE $CONTENT_DIR
 cp $CONFIG_FILE $CONTENT_DIR
 
-docker build -t rpi-reader ${DIR}/docker/
+
+case $CPU_ARCHITECTURE in
+x86_64)
+  docker build -t richodemus/reader:latest -f ${DIR}/docker/Dockerfile ${DIR}/docker/
+  ;;
+armv7l)
+  docker build -t richodemus/reader:latest -f ${DIR}/docker/Dockerfile_arm ${DIR}/docker/
+  ;;
+esac
 
 rm $CONTENT_DIR/*
 rmdir $CONTENT_DIR
