0c4a7d44ed277d055a8eaf58c7267cf09fa91534
RichoDemus <richodemus@gmail.com>
2016-05-06 21:33:12 +0200

    Changed token timeout to 1 month

--- web/src/main/java/com/richo/reader/web/dropwizard/GuiceModule.java
+++ web/src/main/java/com/richo/reader/web/dropwizard/GuiceModule.java
@@ -19,7 +19,7 @@ class GuiceModule extends AbstractModule
 	{
 		install(new BackendModule());
 		bind(UserService.class).to(UserServiceBridge.class);
-		bind(Duration.class).annotatedWith(Names.named("tokenDuration")).toInstance(Duration.ofHours(4));
+		bind(Duration.class).annotatedWith(Names.named("tokenDuration")).toInstance(Duration.ofDays(30L));
 		bind(String.class).annotatedWith(Names.named("secret")).toInstance("qwel12319zxc90we23rnlsdfpsdf09sdfk323rlksdfsd");
 		bind(BackendHealthCheck.class);
 	}
