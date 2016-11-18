d76171660c248409d82bfbfc0036855c7e74bba1
RichoDemus <richodemus@gmail.com>
2016-11-18 22:13:52 +0100

    Played around a bit with fetching video view count

--- youtube-feed-service/src/main/java/com/richo/reader/youtube_feed_service/youtube/YoutubeChannelDownloader.java
+++ youtube-feed-service/src/main/java/com/richo/reader/youtube_feed_service/youtube/YoutubeChannelDownloader.java
@@ -4,12 +4,18 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Channel;
+import com.google.api.services.youtube.model.ChannelSnippet;
+import com.google.api.services.youtube.model.ChannelStatistics;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,11 +35,65 @@ public class YoutubeChannelDownloader
 		final YouTube.Builder builder = new YouTube.Builder(
 				new NetHttpTransport(),
 				new JacksonFactory(),
-				request -> {
+				request ->
+				{
 				})
 				.setApplicationName("Richo-Reader");
 		env_url.ifPresent(builder::setRootUrl);
 		youtube = builder.build();
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		final YoutubeChannelDownloader downloader = new YoutubeChannelDownloader("AIzaSyChI7lMyLfc1ckOqcC-z2Oz-Lrq6d09x30");
+		downloader.printVideoViews();
+		downloader.channelStatistics();
+	}
+
+	private void channelStatistics() throws IOException
+	{
+		final List<Channel> channels = youtube.channels()
+				.list("snippet,status,id,statistics")
+				.setKey(apiKey)
+				.setForUsername("RichoDemus")
+				.execute()
+				.getItems();
+
+		final String channelName = channels.stream()
+				.map(Channel::getSnippet)
+				.map(ChannelSnippet::getTitle)
+				.findAny()
+				.orElse("CHANNELNAME_NOT_FOUND");
+		final BigInteger views = channels.stream()
+				.map(Channel::getStatistics)
+				.map(ChannelStatistics::getViewCount)
+				.findAny()
+				.orElse(BigInteger.ZERO);
+
+		System.out.println(channelName + " has " + views + " views :(");
+	}
+
+	private void printVideoViews() throws IOException
+	{
+
+		final List<Video> items = youtube.videos()
+				.list("snippet,status,id,statistics")
+				.setKey(apiKey)
+				.setId("NVbH1BVXywY")
+				.execute()
+				.getItems();
+
+		final String title = items.stream()
+				.map(Video::getSnippet)
+				.map(VideoSnippet::getTitle)
+				.findAny()
+				.orElse("TITLE_NOT_FOUND");
+		final BigInteger viewCount = items.stream()
+				.map(Video::getStatistics)
+				.map(VideoStatistics::getViewCount)
+				.findAny()
+				.orElse(BigInteger.ZERO);
+		System.out.println(title + " has " + viewCount + " views");
 	}
 
 	public Optional<YoutubeVideoChunk> getVideoChunk(String channelName)
