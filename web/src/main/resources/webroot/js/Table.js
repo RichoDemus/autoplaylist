f001d203df9542ac9452cefa4e6c27fb30bae034
RichoDemus <richodemus@gmail.com>
2016-11-20 19:20:07 +0100

    Replaced some functions with arrow functions

--- web/src/main/resources/webroot/js/Table.js
+++ web/src/main/resources/webroot/js/Table.js
@@ -1,5 +1,4 @@
-var Table = (function()
-{
+var Table = ((()=>{
 	var pub = {},
 	//Private property
 	greyFloorTile = null;
@@ -8,7 +7,7 @@ var Table = (function()
 	//pub.ingredient = "Bacon Strips";
 
 	//Public method
-	pub.init = function ()
+	pub.init = () =>
 	{
 		$('#itemListTable').DataTable({
 			"paging": true,
@@ -18,8 +17,7 @@ var Table = (function()
 		});
 	};
 
-	pub.addItemToTable = function (feedId, item)
-	{
+	pub.addItemToTable = (feedId, item)=>
 		$('#itemListTable').DataTable().row.add([
 			Table.getMarkAsReadToggleButton(feedId, item),
 			Table.getTitle(feedId, item),
@@ -27,10 +25,8 @@ var Table = (function()
 			item.uploadDate,
 			getMarkOlderItemsAsReadButton(feedId, item)
 		]);
-	};
 
-	pub.addFeedToTable = function(feed)
-	{
+	pub.addFeedToTable = feed=>{
 		const count = getNumberOfItems(feed);
 		if(feed.id === selectedFeed)
 		{
@@ -44,72 +40,56 @@ var Table = (function()
 		}
 	};
 
-	pub.addItemsToTable = function()
-	{
+	pub.addItemsToTable = ()=>{
 		if(!selectedFeed)
 		{
 			console.log("No feed selected");
 			return;
 		}
-		feeds.forEach(function(feed)
-		{
+		feeds.forEach(feed=>{
 			if(feed.id === selectedFeed)
 			{
-				feed.items.forEach(function(item)
-				{
-					Table.addItemToTable(feed.id, item);
-				});
+				feed.items.forEach(item=>Table.addItemToTable(feed.id, item));
 				$('#itemListTable').DataTable().draw();
 			}
 		});
 	};
 
-	pub.addFeedsToTable = function()
+	pub.addFeedsToTable = ()=>feeds.forEach(feed=>
 	{
-		feeds.forEach(function(feed)
-		{
-			if(selectedLabel && selectedLabel.id === UNLABELED_LABEL.id)
-			{
-				//add feed if it doesnt belong in any label
-				var hasLabel = false;
-				for(var i = 0; i < labels.length; i++)
-				{
-					var label = labels[i];
-					console.log(label);
-					if(label.feeds.indexOf(feed.id) > -1)
-					{
-						//this feed belong to a label
-						console.log("Feed [" + feed.name + "] is in label [" + label.name + "]");
-						hasLabel = true;
-						break;
-					}
-				}
-				if(!hasLabel)
-				{
-					console.log("Feed [" + feed.name + "] is not in any label");
-					Table.addFeedToTable(feed);
+		if (selectedLabel && selectedLabel.id === UNLABELED_LABEL.id) {
+			//add feed if it doesnt belong in any label
+			var hasLabel = false;
+			for (var i = 0; i < labels.length; i++) {
+				var label = labels[i];
+				console.log(label);
+				if (label.feeds.indexOf(feed.id) > -1) {
+					//this feed belong to a label
+					console.log("Feed [" + feed.name + "] is in label [" + label.name + "]");
+					hasLabel = true;
+					break;
 				}
 			}
-			else if((selectedLabel && selectedLabel.feeds.indexOf(feed.id) > -1) || !selectedLabel)
-			{
+			if (!hasLabel) {
+				console.log("Feed [" + feed.name + "] is not in any label");
 				Table.addFeedToTable(feed);
 			}
-			else if(selectedLabel && selectedLabel === ALL_LABEL)
-			{
-				Table.addFeedToTable(feed);
-			}
-		});
-	};
+		}
+		else if ((selectedLabel && selectedLabel.feeds.indexOf(feed.id) > -1) || !selectedLabel) {
+			Table.addFeedToTable(feed);
+		}
+		else if (selectedLabel && selectedLabel === ALL_LABEL) {
+			Table.addFeedToTable(feed);
+		}
+	});
 
-	pub.clearTables = function()
-	{
+	pub.clearTables = ()=>{
 		$("#feedListTable").find("tbody tr").remove();
 		$("#labelListTable").find("tbody tr").remove();
 		$('#itemListTable').dataTable().fnClearTable();
 	};
 
-	pub.addLabelToTable = function(label)
-	{
+	pub.addLabelToTable = label=>{
 		if(selectedLabel && label.id === selectedLabel.id)
 		{
 			$('#labelListTable').find('> tbody:last')
@@ -122,29 +102,21 @@ var Table = (function()
 		}
 	};
 
-	pub.addLabelsToTable = function()
-	{
-		labels.forEach(function(label)
-		{
-			Table.addLabelToTable(label);
-		});
-	};
+	pub.addLabelsToTable = ()=>labels.forEach(label=>Table.addLabelToTable(label));
 
-	pub.getMarkAsReadToggleButton = function(feedId, item)
-	{
-		return "<button id=\"markItemAsReadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id + "\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markAsReadButtonPressed(this)\">Mark as read</button>" +
-			"<button id=\"markItemAsUnreadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id + "\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markAsUnreadButtonPressed(this)\" style=\"display:none;\" >Mark as unread</button>";
-	};
+	pub.getMarkAsReadToggleButton = (feedId, item)=>"<button id=\"markItemAsReadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id +
+	"\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markAsReadButtonPressed(this)\">Mark as read</button>" +
+	"<button id=\"markItemAsUnreadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id +
+	"\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markAsUnreadButtonPressed(this)\" style=\"display:none;\" >Mark as unread</button>";
 
-	pub.getTitle = function(feedId, item)
-	{
-		return "<a id=\"" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id + "\" href=\"" + item.url + "\" target=\"_blank\" onClick=\"Buttons.itemTitleClicked(this)\">" + item.title + "</a>";
-	};
+	pub.getTitle = (feedId, item)=>"<a id=\"" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id + "\" href=\"" + item.url +
+	"\" target=\"_blank\" onClick=\"Buttons.itemTitleClicked(this)\">" + item.title + "</a>";
 
 	//Private method
 	function getMarkOlderItemsAsReadButton(feedId, item)
 	{
-		return "<button id=\"markAllOlderItemsAsReadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id + "\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markOlderItemsAsReadButtonPressed(this)\">Mark older as read</button>"
+		return "<button id=\"markAllOlderItemsAsReadButton-" + item.id + "\" data-feed-id=\"" + feedId + "\" data-id=\"" + item.id +
+			"\" type=\"button\" class=\"btn btn-xs btn-default\" onClick=\"Buttons.markOlderItemsAsReadButtonPressed(this)\">Mark older as read</button>"
 	}
 
 	//todo this should be a method of the feed object
@@ -161,4 +133,4 @@ var Table = (function()
 	*/
 	//Return just the public parts
 	return pub;
-}());
+})());
