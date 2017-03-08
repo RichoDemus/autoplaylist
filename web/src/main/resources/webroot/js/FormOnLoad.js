dbd8690fa73c775f33ebf0db523504430167bf67
RichoDemus <richodemus@gmail.com>
2017-03-08 19:08:08 +0100

    Added skeleton for admin page

--- web/src/main/resources/webroot/js/FormOnLoad.js
+++ web/src/main/resources/webroot/js/FormOnLoad.js
@@ -2,5 +2,9 @@ $(function()
 {
 	Authentication.setLoginFormBehaviour();
 	Authentication.setSignupFormBehaviour();
+	$('#adminModal').on('shown.bs.modal', function (e) {
+	  alert("Modal opened")
+	});
+
 	Service.init();
 });
