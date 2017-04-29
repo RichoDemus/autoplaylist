7e8be38953c41aeba0ab2f5bac867e9eb9e93c13
RichoDemus <richodemus@gmail.com>
2017-04-29 17:08:10 +0200

    No longer possible to use "valid" tokens with a non existing user

--- server/core/web/src/main/java/com/richo/reader/web/authentication/UsernameCheckFilter.java
+++ server/core/web/src/main/java/com/richo/reader/web/authentication/UsernameCheckFilter.java
@@ -4,6 +4,8 @@ import com.google.common.base.Strings;
 import com.richodemus.dropwizard.jwt.AuthenticationManager;
 import com.richodemus.dropwizard.jwt.RawToken;
 import com.richodemus.dropwizard.jwt.Token;
+import com.richodemus.reader.dto.Username;
+import com.richodemus.reader.user_service.UserService;
 import org.glassfish.jersey.server.ContainerRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,11 +31,13 @@ public class UsernameCheckFilter implements ContainerRequestFilter
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final AuthenticationManager authenticationManager;
+	private final UserService userService;
 
 	@Inject
-	public UsernameCheckFilter(final AuthenticationManager authenticationManager)
+	public UsernameCheckFilter(final AuthenticationManager authenticationManager, final UserService userService)
 	{
 		this.authenticationManager = authenticationManager;
+		this.userService = userService;
 	}
 
 	@Override
@@ -48,12 +52,14 @@ public class UsernameCheckFilter implements ContainerRequestFilter
 			logger.debug("Not a user resource");
 			return;
 		}
+
 		final String[] split = path.split("/");
 		if (split.length < 3)
 		{
 			logger.debug("No username in path");
 			return;
 		}
+
 		final String usernameFromURI = split[1];
 		final String rawToken = requestContext.getHeaderString("x-token-jwt");
 		if (Strings.isNullOrEmpty(rawToken))
@@ -61,11 +67,18 @@ public class UsernameCheckFilter implements ContainerRequestFilter
 			logger.debug("no token");
 			return;
 		}
+
 		final Token token = authenticationManager.parseToken(new RawToken(rawToken));
 		final String usernameFromToken = token.getUsername();
 		if (!usernameFromToken.equals(usernameFromURI))
 		{
 			logger.warn("username from token {} does not match username in url {}", usernameFromToken, usernameFromURI);
+			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+		}
+
+		if (userService.find(new Username(usernameFromToken)) == null)
+		{
+			logger.warn("username from token {} does not exist", usernameFromToken);
 			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
 		}
 	}
--- server/test/reader-test/src/component-test/java/com/richo/reader/test/SecurityTestIT.java
+++ server/test/reader-test/src/component-test/java/com/richo/reader/test/SecurityTestIT.java
@@ -73,4 +73,17 @@ public class SecurityTestIT
 		// attempt to get feeds for another account than the token is issued for
 		assertThatThrownBy(() -> hackingAttemptPage.toFeedPage().getAllFeeds()).isInstanceOf(AssertionError.class).hasMessageContaining("Expected status code <200> doesn't match actual status code <401>.");
 	}
+
+		@Test
+		public void shouldNotBePossibleToUseWithValidTokenButUnownUserName() throws Exception
+		{
+			// this is a token with user: not_richodemus
+			final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTYwNjgzNDcsInVzZXIiOiJub3RfcmljaG9kZW11cyIsInJvbGUiOiJ1c2VyIn0.KOYnbURz-qco0uVLUESBcWWRJaOVYu3Ti0_IWaI-H4Q";
+
+			loginPage.setUsername("not_richodemus");
+			loginPage.setToken(token);
+
+			assertThatThrownBy(loginPage::refreshToken);
+		}
+
 }
