98a5f820898cca4d899480b93dfd68780866a05c
RichoDemus <richodemus@gmail.com>
2018-07-11 23:40:56 +0200

    Return http 403(FORBIDDEN) if session doesn't have an access token

--- autoplaylist-backend/src/main/kotlin/com.richodemus/autoplaylist/Application.kt
+++ autoplaylist-backend/src/main/kotlin/com.richodemus/autoplaylist/Application.kt
@@ -1,11 +1,15 @@
 package com.richodemus.autoplaylist
 
+import io.github.vjames19.futures.jdk8.Future
 import io.github.vjames19.futures.jdk8.flatMap
 import io.github.vjames19.futures.jdk8.map
 import io.github.vjames19.futures.jdk8.recover
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -40,25 +44,30 @@ open class Application {
     internal data class CreateSessionResponse(val msg: String)
 
     @GetMapping("/users/me")
-    internal fun getUser(session: HttpSession): CompletableFuture<UserId> {
-        val accessToken = session.getAttribute("accessToken") as AccessToken
+    internal fun getUser(session: HttpSession): CompletableFuture<ResponseEntity<UserId>> {
+        val accessToken = session.getAttribute("accessToken") as AccessToken?
+                ?: return Future { ResponseEntity<UserId>(FORBIDDEN) }
         logger.info("Session {} with token {}", session.id, accessToken)
         return getUserIdMemoized(accessToken)
+                .map { ResponseEntity(it, OK) }
     }
 
     @GetMapping("/playlists")
-    internal fun getPlaylists(session: HttpSession): CompletableFuture<List<PlayList>> {
-        val accessToken = session.getAttribute("accessToken") as AccessToken
-        return getPlaylists(accessToken).map { it.items }
+    internal fun getPlaylists(session: HttpSession): CompletableFuture<ResponseEntity<List<PlayList>>> {
+        val accessToken = session.getAttribute("accessToken") as AccessToken?
+                ?: return Future { ResponseEntity<List<PlayList>>(FORBIDDEN) }
+        return getPlaylists(accessToken)
+                .map { ResponseEntity(it.items, OK) }
     }
 
     @PostMapping("/playlists")
     internal fun createPlayList(
             session: HttpSession,
             @RequestBody request: CreatePlaylistRequest
-    ): CompletableFuture<CreatePlaylistResponse> {
+    ): CompletableFuture<ResponseEntity<CreatePlaylistResponse>> {
         logger.info("Asked to create playlist {} with tracks by {}", request.name, request.artist)
-        val accessToken = session.getAttribute("accessToken") as AccessToken
+        val accessToken = session.getAttribute("accessToken") as AccessToken?
+                ?: return Future { ResponseEntity<CreatePlaylistResponse>(FORBIDDEN) }
         return getUserIdMemoized(accessToken)
                 .flatMap { userId ->
                     createPlayListFromArtist(accessToken, userId, request.name, request.artist)
@@ -73,10 +82,11 @@ open class Application {
                     logger.info("Failed to create playlist {}", request, e)
                     CreatePlaylistResponse(false)
                 }
+                .map { ResponseEntity(it, OK) }
 
     }
 
-    private val getUserIdMemoized = { accessToken: AccessToken -> getUserId(accessToken)}.memoize()
+    private val getUserIdMemoized = { accessToken: AccessToken -> getUserId(accessToken) }.memoize()
 }
 
 data class CreateSessionRequest(val code: String)
