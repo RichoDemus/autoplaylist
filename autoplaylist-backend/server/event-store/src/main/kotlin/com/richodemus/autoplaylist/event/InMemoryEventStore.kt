e62f95986bf79faca3cd5908ac1a0517ddcfa9c7
RichoDemus <richodemus@gmail.com>
2018-07-18 12:06:26 +0200

    Add metric for number of temporary listeners

--- autoplaylist-backend/server/event-store/src/main/kotlin/com/richodemus/autoplaylist/event/InMemoryEventStore.kt
+++ autoplaylist-backend/server/event-store/src/main/kotlin/com/richodemus/autoplaylist/event/InMemoryEventStore.kt
@@ -1,13 +1,19 @@
 package com.richodemus.autoplaylist.event
 
+import io.micrometer.core.instrument.MeterRegistry
+import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
 @Named
-internal class InMemoryEventStore : EventStore {
+internal class InMemoryEventStore @Inject constructor(registry: MeterRegistry) : EventStore {
     private val messageListeners = mutableListOf<(Event) -> Unit>()
     private val temporaryListeners = mutableListOf<(Event) -> Boolean>()
+
+    init {
+        registry.gauge("temporary.listeners", temporaryListeners) { it.size.toDouble() }
+    }
 
     override fun consume(messageListener: (Event) -> Unit) {
         this.messageListeners.add(messageListener)
