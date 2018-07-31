539b84e076e10d3504239124f96542a71c15f629
RichoDemus <richodemus@gmail.com>
2018-07-31 16:37:35 +0200

    Add test for getting playlists

--- autoplaylist-backend/server/spring/src/test/kotlin/com/richodemus/autoplaylist/test/ServiceTest.kt
+++ autoplaylist-backend/server/spring/src/test/kotlin/com/richodemus/autoplaylist/test/ServiceTest.kt
@@ -1,10 +1,14 @@
 package com.richodemus.autoplaylist.test
 
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.whenever
 import com.richodemus.autoplaylist.dto.RefreshToken
 import com.richodemus.autoplaylist.dto.SpotifyUserId
 import com.richodemus.autoplaylist.spotify.AccessToken
+import com.richodemus.autoplaylist.spotify.PlayList
+import com.richodemus.autoplaylist.spotify.PlayListId
+import com.richodemus.autoplaylist.spotify.PlaylistName
 import com.richodemus.autoplaylist.spotify.SpotifyPort
 import com.richodemus.autoplaylist.spotify.Tokens
 import com.richodemus.autoplaylist.test.pages.LoginPage
@@ -17,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
+import java.util.UUID
 
 
 @RunWith(SpringRunner::class)
@@ -31,10 +36,16 @@ internal class ServiceTest {
 
     private lateinit var loginPage: LoginPage
 
+    private lateinit var oAuth2Code: String
+
+    private lateinit var spotifyUserId: String
+
     @Before
     fun setUp() {
+        oAuth2Code = UUID.randomUUID().toString()
+        spotifyUserId = UUID.randomUUID().toString()
         loginPage = LoginPage(port)
-        whenever(spotifyPort.getToken("my-fancy-code")).doReturn(Future {
+        whenever(spotifyPort.getToken(oAuth2Code)).doReturn(Future {
             Tokens(
                     AccessToken("access-token"),
                     "scope",
@@ -44,15 +55,43 @@ internal class ServiceTest {
             )
         })
         whenever(spotifyPort.getUserId(AccessToken("access-token"))).doReturn(Future {
-            SpotifyUserId("spotify-user-id")
+            SpotifyUserId(spotifyUserId)
+        })
+        whenever(spotifyPort.refreshToken(any())).doReturn(Future {
+            Tokens(
+                    AccessToken("access-token"),
+                    "scope",
+                    "type",
+                    100000,
+                    RefreshToken("refresh-token")
+            )
+        })
+        whenever(spotifyPort.getPlaylists(any())).doReturn(Future {
+            listOf(
+                    PlayList(PlayListId("id1"), PlaylistName("name1")),
+                    PlayList(PlayListId("id2"), PlaylistName("name2"))
+            )
         })
     }
 
     @Test
     fun `Register new user`() {
-        val mainPage = loginPage.login("my-fancy-code")
-        val spotifyUserId = mainPage.getSpotifyUserId()
+        val mainPage = loginPage.login(oAuth2Code)
+        val result = mainPage.getSpotifyUserId()
 
-        assertThat(spotifyUserId).isEqualTo(SpotifyUserId("spotify-user-id"))
+        assertThat(result).isEqualTo(SpotifyUserId(spotifyUserId))
+    }
+
+    @Test
+    fun `Get playlists`() {
+        val mainPage = loginPage.login(oAuth2Code)
+        val result = mainPage.getPlaylists()
+
+        val expected = listOf(
+                PlayList(PlayListId("id1"), PlaylistName("name1")),
+                PlayList(PlayListId("id2"), PlaylistName("name2"))
+        )
+
+        assertThat(result).isEqualTo(expected)
     }
 }
--- autoplaylist-backend/server/spring/src/test/kotlin/com/richodemus/autoplaylist/test/pages/MainPage.kt
+++ autoplaylist-backend/server/spring/src/test/kotlin/com/richodemus/autoplaylist/test/pages/MainPage.kt
@@ -1,6 +1,7 @@
 package com.richodemus.autoplaylist.test.pages
 
 import com.richodemus.autoplaylist.dto.SpotifyUserId
+import com.richodemus.autoplaylist.spotify.PlayList
 import io.restassured.RestAssured
 import io.restassured.http.Cookie
 
@@ -12,6 +13,13 @@ class MainPage(private val port: Int, private val sessionId: Cookie) {
                 .then().assertThat().statusCode(200).extract().jsonPath().getString("userId")
 
         return SpotifyUserId(userId)
+    }
+
+    fun getPlaylists(): List<PlayList> {
+        return RestAssured
+                .given().cookie(sessionId)
+                .`when`().get("http://localhost:$port/v1/playlists")
+                .then().assertThat().statusCode(200).extract().jsonPath().getList("", PlayList::class.java)
     }
 
 }
