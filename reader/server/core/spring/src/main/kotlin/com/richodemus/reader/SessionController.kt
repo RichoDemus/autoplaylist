c0843ccc3c08224ca2b1d811b2d3ab029069d015
RichoDemus <git@richodemus.com>
2020-07-13 20:44:02 +0200

    Keep sessions valid for 90 days

--- reader/server/core/spring/src/main/kotlin/com/richodemus/reader/SessionController.kt
+++ reader/server/core/spring/src/main/kotlin/com/richodemus/reader/SessionController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import userId
 import username
+import java.time.Duration
 import javax.servlet.http.HttpSession
 
 /**
@@ -49,6 +50,7 @@ internal class SessionController(
                 val id = userService.find(request.username)!!.id
                 session.userId = id
                 session.username = request.username
+                session.maxInactiveInterval = Duration.ofDays(30).seconds.toInt()
                 ResponseEntity.ok("OK")
             } else {
                 ResponseEntity(UNAUTHORIZED)
