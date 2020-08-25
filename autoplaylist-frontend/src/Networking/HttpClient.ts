5ed5d825d0c25f18c880d6830e65669108694ba5
RichoDemus <git@richodemus.com>
2020-08-25 19:55:29 +0200

    Add v1 to url again

--- autoplaylist-frontend/src/Networking/HttpClient.ts
+++ autoplaylist-frontend/src/Networking/HttpClient.ts
@@ -4,7 +4,7 @@ const getBackendBaseUrl = () => {
     if (window.location.hostname === "localhost") {
         return "http://localhost:8080/v1"
     }
-    return "https://api.autoplaylist.richodemus.com"
+    return "https://api.autoplaylist.richodemus.com/v1"
 };
 
 export const checkForValidSession = () => {
