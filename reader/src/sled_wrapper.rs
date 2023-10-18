434ecc7b30962741ff50693882758c165b73b1c8
RichoDemus <git@richodemus.com>
2023-10-18 23:10:26 +0200

    Add proper types to DiskCache

--- reader/src/sled_wrapper.rs
+++ reader/src/sled_wrapper.rs
@@ -2,22 +2,24 @@ use log::warn;
 use serde::{Deserialize, Serialize};
 use sled::Db;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 use std::ops::Deref;
 
-pub struct DiskCache {
+pub struct DiskCache<K, V> {
     sled: Db,
+    key_type: PhantomData<K>,
+    value_type: PhantomData<V>,
 }
 
-impl DiskCache {
+impl<K: Deref<Target = String>, V: Serialize + Debug + for<'a> Deserialize<'a>> DiskCache<K, V> {
     pub fn new(name: &str) -> Self {
         Self {
             sled: sled::open(format!("target/{}_db", name)).unwrap(),
+            key_type: PhantomData,
+            value_type: PhantomData,
         }
     }
-    pub fn get<K: Deref<Target = String>, V: Debug + for<'a> Deserialize<'a>>(
-        &self,
-        key: K,
-    ) -> Option<V> {
+    pub fn get(&self, key: K) -> Option<V> {
         self.sled
             .get(key.deref())
             .expect("Failed to read from disk")
@@ -33,7 +35,7 @@ impl DiskCache {
             .flatten()
     }
 
-    pub fn insert<K: Deref<Target = String>, V: Serialize>(&self, key: K, value: V) {
+    pub fn insert(&self, key: K, value: V) {
         let _ = self
             .sled
             .insert(key.deref(), serde_json::to_vec(&value).unwrap());
@@ -48,7 +50,7 @@ mod tests {
 
     #[test]
     fn test_cache() {
-        let cache = DiskCache::new("test");
+        let cache: DiskCache<ChannelId, Channel> = DiskCache::new("test");
         let id = ChannelId(Uuid::new_v4().to_string());
         let name = ChannelName(Uuid::new_v4().to_string());
         let playlist = PlaylistId(Uuid::new_v4().to_string());
@@ -58,8 +60,8 @@ mod tests {
             playlist,
         };
 
-        assert!(cache.get::<ChannelId, Channel>(id.clone()).is_none());
+        assert!(cache.get(id.clone()).is_none());
         cache.insert(id.clone(), feed.clone());
-        assert_eq!(cache.get::<ChannelId, Channel>(id), Some(feed));
+        assert_eq!(cache.get(id), Some(feed));
     }
 }
