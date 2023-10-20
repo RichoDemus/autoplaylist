fcfca916a20c3c8968ea8c3a5da18f333cfe71e5
RichoDemus <git@richodemus.com>
2023-10-20 15:52:54 +0200

    Use httpmock matches to remove ordering requirement

--- reader/src/test/youtube_mock.rs
+++ reader/src/test/youtube_mock.rs
@@ -1,4 +1,5 @@
 use httpmock::prelude::*;
+use log::trace;
 use serde_json::json;
 use std::{env, mem};
 
@@ -67,36 +68,13 @@ impl YoutubeMock {
                 .query_param("key", "YT_KEY")
                 .query_param("part", "snippet")
                 .query_param("playlistId", "uploads-playlist-id")
-                .query_param("page_token", "page-two");
-            then.status(200).json_body(json!({
-                "items": [{
-                    "snippet": {
-                        "resourceId": {
-                            "videoId": "video3-id"
-                        },
-                        "title": "video3-title",
-                        "description": "video3-desc",
-                        "publishedAt": "2013-10-15T00:59:50Z"
-                    }
-                },{
-                    "snippet": {
-                        "resourceId": {
-                            "videoId": "video4-id"
-                        },
-                        "title": "video4-title",
-                        "description": "video4-desc",
-                        "publishedAt": "2012-10-15T00:59:50Z"
-                    }
-                }],
-            }));
-        });
-
-        self.mock_server.mock(|when, then| {
-            when.method(GET)
-                .path("/youtube/v3/playlistItems/")
-                .query_param("key", "YT_KEY")
-                .query_param("part", "snippet")
-                .query_param("playlistId", "uploads-playlist-id");
+                .matches(|m| {
+                    !m.query_params
+                        .clone()
+                        .unwrap_or_default()
+                        .iter()
+                        .any(|(h, _v)| h == "page_token")
+                });
             then.status(200).json_body(json!({
                 "items": [{
                     "snippet": {
@@ -118,6 +96,36 @@ impl YoutubeMock {
                     }
                 }],
                 "nextPageToken": "page-two",
+            }));
+        });
+
+        self.mock_server.mock(|when, then| {
+            when.method(GET)
+                .path("/youtube/v3/playlistItems/")
+                .query_param("key", "YT_KEY")
+                .query_param("part", "snippet")
+                .query_param("playlistId", "uploads-playlist-id")
+                .query_param("page_token", "page-two");
+            then.status(200).json_body(json!({
+                "items": [{
+                    "snippet": {
+                        "resourceId": {
+                            "videoId": "video3-id"
+                        },
+                        "title": "video3-title",
+                        "description": "video3-desc",
+                        "publishedAt": "2013-10-15T00:59:50Z"
+                    }
+                },{
+                    "snippet": {
+                        "resourceId": {
+                            "videoId": "video4-id"
+                        },
+                        "title": "video4-title",
+                        "description": "video4-desc",
+                        "publishedAt": "2012-10-15T00:59:50Z"
+                    }
+                }],
             }));
         });
     }
